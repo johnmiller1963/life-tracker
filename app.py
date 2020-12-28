27643dcae1ad362a371d0dc0dedaa76118d0de57
johnmiller1963 <john@ritmanager.co.uk>
2020-12-28 14:09:09 +0000

    first call to mongo documents

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-@app.route('/')
-def index():
-    return 'Hello World123'
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/get_items")
+def get_items():
+    return "Hello World"
+    items = mongo.db.tbl_items.find()
+    return render_template("items.html", items=items)
 
 
 if __name__ == "__main__":
