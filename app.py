1005d703909d7b1e868d9bae6ff66c05ff33e7d6
johnmiller1963 <john@ritmanager.co.uk>
2020-12-28 15:27:32 +0000

    connection to mongo working correctly

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_items")
 def get_items():
-    return "I am in the app.py file"
     items = mongo.db.tbl_items.find()
     return render_template("items.html", items=items)
 
