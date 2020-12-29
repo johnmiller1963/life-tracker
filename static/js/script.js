5d61d7097de16570854ac588e15a49242774e480
johnmiller1963 <john@ritmanager.co.uk>
2020-12-29 19:52:34 +0000

    hero image implemented

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+M.AutoInit();
+
+var instance = M.Carousel.init({
+fullWidth: true,
+indicators: true
+});
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
