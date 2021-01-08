3f03689a523c302a207a1690ebaa7c28094795ae
johnmiller1963 <john@ritmanager.co.uk>
2021-01-08 14:10:58 +0000

    register template implemented

--- app.py
+++ app.py
@@ -24,6 +24,63 @@ def show_home():
     return render_template("home.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if user_password already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"user_email": request.form.get("user_email").lower()})
+
+        if existing_user:
+            flash("User already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "useremail": request.form.get("user_email").lower(),
+            "username": request.form.get("user_display_name").lower(),
+            "password": generate_password_hash(request.form.get("user_password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("user_email").lower()
+        flash("Registration Successful!")
+        #after correct registration direct user to add new items page NOT profile
+        #return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 @app.route("/get_items")
 def get_items():
     items = mongo.db.tbl_items.find()
