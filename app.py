10f086df78235bfd08d37f8f3a0e5e073dfbfd32
johnmiller1963 <john@ritmanager.co.uk>
2021-01-29 09:08:36 +0000

    commenting check for all pages except ReadMe, final check via Heroku

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+""" Main file for the Life Tracker system, incorporating
+    routing, logic and MongoDB interaction
+"""
+
 import os
 from dateutil.parser import parse
 from datetime import datetime
-# import locale
-# locale.setlocale( locale.LC_ALL, '' )
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
@@ -35,8 +37,9 @@ def register():
             {"user_email": request.form.get("user_email").lower()})
 
         if existing_user:
-            flash("User email already registered, please use a different email!")
-            # print("User email already registered, please use a different email!")
+            flash('User email already registered, please \
+                use a different email!')
+
             return redirect(url_for("register"))
 
         register = {
@@ -52,17 +55,8 @@ def register():
 
         # put the new user into 'session' cookie
         session["user_email"] = request.form.get("user_email").lower()
-        #current_user_email = request.form.get("user_email").lower()
-        #return render_template('base.html', current_user_name=request.form.get("user_email").lower())
-
-        #sessionStorage.setItem('key', 'value')
-        flash("User Registration Successful!")
         flash("Welcome, {}".format(
             request.form.get("user_display_name")))
-        #print("User Registration Successful!")
-
-        # after correct registration direct user to add new items page NOT profile
-        # return redirect(url_for("profile", username=session["user"]))
         return redirect(url_for("items", username=session["user_email"]))
     return render_template("register.html")
 
@@ -70,23 +64,20 @@ def register():
 @app.route("/logon", methods=["GET", "POST"])
 def logon():
     if request.method == "POST":
-        # check if user email exists in db
+        # check if user email exists in mongo
         existing_user = mongo.db.tbl_users.find_one(
             {"user_email": request.form.get("user_email").lower()})
-        #print("The next value is the existing_user variable for email...")
-        #print(request.form.get("user_email").lower())
-        #print(existing_user["user_email"])
-        #print(existing_user["user_display_name"])
-        #print(existing_user["user_password_hash"])
-        #flash({{existing_email["user_display_name"]}})
+
         if existing_user:
             # check hashed password matches user input
-            #flash("Found existing user")
-            if check_password_hash(existing_user["user_password_hash"], request.form.get("user_password")):
+            if check_password_hash(
+                existing_user["user_password_hash"], request.form.get(
+                    "user_password")):
                 session["user_email"] = request.form.get("user_email").lower()
                 flash("Welcome back, {}".format(
                     existing_user["user_display_name"]))
-                return redirect(url_for("items", username=session["user_email"]))
+                return redirect(url_for(
+                    "items", username=session["user_email"]))
             else:
                 # invalid password match
                 flash("Incorrect Email and/or Password")
@@ -105,8 +96,6 @@ def demo():
     session.clear()
     session["user_email"] = "demo@life-tracker.co.uk"
     flash("Now viewing our Demo account")
-    #flash(current_user_email)
-    # flash(session["user_email"])
     return redirect(url_for("items", username=session["user_email"]))
 
 
@@ -121,16 +110,17 @@ def logout():
 def items():
     if request.method == "POST":
         if request.form.get("new_item_expiry_date"):
-            optional_expiry_date = parse(request.form.get("new_item_expiry_date"))
+            optional_expiry_date = parse(request.form.get(
+                "new_item_expiry_date"))
         else:
             optional_expiry_date = ""
 
+        # This switch is important as it places items in the correct tab
         if request.form.get("new_item_recurs_months"):
             optional_recurs = (request.form.get("new_item_recurs_months"))
         else:
             optional_recurs = "0"
 
-        # print(optional_expiry_date)
         one_new_item = {
             "item_user_email": session["user_email"].lower(),
             "item_title": request.form.get("new_item_title"),
@@ -145,14 +135,34 @@ def items():
         flash("New item added successfully!")
         return redirect(url_for("items", username=session["user_email"]))
 
-    items_renewables = list(mongo.db.tbl_items.find({"item_user_email": session["user_email"], "item_hide": "off", "item_recurs_months": {"$gt": "0"}}).sort([("item_expiry_date", 1)]))
-    items_warranties = list(mongo.db.tbl_items.find({"item_user_email": session["user_email"], "item_hide": "off", "item_recurs_months": "0"}).sort([("item_expiry_date", 1)]))
-    return render_template("items.html", items_renewables=items_renewables, items_warranties=items_warranties)
+    items_renewables = list(
+        mongo.db.tbl_items.find(
+            {
+                "item_user_email": session["user_email"],
+                "item_hide": "off",
+                "item_recurs_months": {"$gt": "0"},
+            }
+        ).sort([("item_expiry_date", 1)])
+    )
+    items_warranties = list(
+        mongo.db.tbl_items.find(
+            {
+                "item_user_email": session["user_email"],
+                "item_hide": "off",
+                "item_recurs_months": "0",
+            }
+        ).sort([("item_expiry_date", 1)])
+    )
+    return render_template(
+        "items.html",
+        items_renewables=items_renewables,
+        items_warranties=items_warranties,
+    )
 
 
+# Defined as a delete function, we are actually only 'hiding' the record
 @app.route("/delete_item/<item_id>")
 def delete_item(item_id):
-    # flash("In app.py delete_item function")
     if session["user_email"] == "demo@life-tracker.co.uk":
         flash("The demo account cannot delete items!")
     else:
@@ -168,25 +178,19 @@ def delete_item(item_id):
 @app.route("/edit_item/<item_id>", methods=["GET", "POST"])
 def edit_item(item_id):
     if request.method == "POST":
-        # flash("Running the POST code now")
-        # request.data
-        # flash(item_id)
-        # test = request.form.get("item_title" + item_id)
-        # test = request.form["item_title" + item_id, ""]
-        # flash(test)
-        # return redirect(url_for("items", username=session["user_email"]))
-        # flash("editing" + item_id):
+        # This would be stopped by local JS, this just to be sure
         if session["user_email"] == "demo@life-tracker.co.uk":
             flash("The demo account cannot edit items!")
         else:
-            # flash("Edit any of the green cells, click save to keep the changes, F5 to cancel")
             if request.form.get("item_expiry_date" + item_id):
-                optional_expiry_date = parse(request.form.get("item_expiry_date" + item_id))
+                optional_expiry_date = parse(request.form.get(
+                    "item_expiry_date" + item_id))
             else:
                 optional_expiry_date = ""
 
             if request.form.get("item_recurs_months" + item_id):
-                optional_recurs = (request.form.get("item_recurs_months" + item_id))
+                optional_recurs = (request.form.get(
+                    "item_recurs_months" + item_id))
             else:
                 optional_recurs = "0"
 
@@ -196,9 +200,11 @@ def edit_item(item_id):
 
             edited_values = {"$set": {
                 "item_title": request.form.get("item_title" + item_id),
-                "item_description": request.form.get("item_description" + item_id),
+                "item_description": request.form.get(
+                    "item_description" + item_id),
                 "item_cost": request.form.get("item_cost" + item_id),
-                "item_start_date": parse(request.form.get("item_start_date" + item_id)),
+                "item_start_date": parse(request.form.get(
+                    "item_start_date" + item_id)),
                 "item_expiry_date": optional_expiry_date,
                 "item_hide": "off",
                 "item_recurs_months": str(optional_recurs)}
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,8 @@
 // First line auto initialises all Materialize components
 M.AutoInit();
 $(document).ready(function() {
+    // Character counters enabled for 'new item' only, not editing item, 
+    // though max character limits are still in place
     $('input#new_item_title, textarea#new_item_description').characterCounter();
     $('.datepicker').datepicker({'format': 'yyyy-mm-dd'});
-    // $('.datepicker').setDefaults({ beforeShow: function (i) { if ($(i).attr('readonly')) { return false; } } });
-    if ($('.datepicker').prop('readonly')) {
-        $('#ui-datepicker-div').css({ 'visibility': 'hidden' })
-    } else { $('#ui-datepicker-div').css({ 'visibility': 'visible' }) }
 });
